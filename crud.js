f6c8a4d9027be4bb0153f32a03379dc195dd979b
Joe Basquez <jybasquez@yahoo.com>
2021-02-28 15:11:50 -0500

    added in connection and connected file to employee database

--- crud.js
+++ crud.js
@@ -1,1 +1,11 @@
+//package requirements
 const mysql = require('mysql');
+
+//connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 8888,
+    user: 'root',
+    password: '12345678',
+    database: 'employeeDB'
+});
