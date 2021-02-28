8d4d3b569b1ec203d8b2964b409f7f5b673947da
Joe Basquez <jybasquez@yahoo.com>
2021-02-28 14:56:46 -0500

    created schema file, employee data base and department table with rows

---
+++ schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS employeeDB;
+CREATE database employeeDB;
+
+USE epmloyeeDB;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NULL
+  PRIMARY KEY (position)
+);
+
+SELECT * FROM top5000;
