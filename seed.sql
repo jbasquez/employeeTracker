f14cddcbaeb6582843656e2564d7664b5547592b
Joe Basquez <jybasquez@yahoo.com>
2021-03-02 19:54:31 -0500

    finished seed.sql file

--- seed.sql
+++ seed.sql
@@ -19,3 +19,18 @@ VALUES
     ('Accountant', 125000, 3),
     ('Legal Team Lead', 250000, 4),
     ('Lawyer', 190000, 4);
+
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('John', 'Doe', 1, NULL),
+    ('Mike', 'Chan', 2, 1),
+    ('Ashley', 'Rodriguez', 3, NULL),
+    ('Kevin', 'Tupik', 4, 3),
+    ('Kunal', 'Singh', 5, NULL),
+    ('Malia', 'Brown', 6, 5),
+    ('Sarah', 'Lourd', 7, NULL),
+    ('Tom', 'Allen', 8, 7);
+Shared in
+homework
+1 day ago
